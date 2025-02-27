22d3b246e1be184724a13ae857219a31cdd5f331
congdinh88 <congdinh.tk@gmail.com>
2025-02-27 23:07:09 +0700

    27/02/25

---
+++ UIStyles/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace UIStyles;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+        customDataGrid.ItemsSource = GetData();
+    }
+    private List<Employee> GetData()
+    {
+        return new List<Employee>
+            {
+                new Employee { Id = 1, Name = "Nguyễn Văn A", Age = 25, Position = "Developer" },
+                new Employee { Id = 2, Name = "Trần Thị B", Age = 30, Position = "Designer" },
+                new Employee { Id = 3, Name = "Phạm Văn C", Age = 28, Position = "Tester" }
+            };
+    }
+}
+public class Employee
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public string Position { get; set; }
+}
